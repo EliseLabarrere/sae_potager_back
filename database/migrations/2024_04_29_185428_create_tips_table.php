b2290b53d362d2cd941d2d44e46c785f7a590ae3
EliseLabarrere <lab.elise@outlook.fr>
2024-04-29 21:12:37 +0200

    feat: add Keywords and Tips to database

---
+++ database/migrations/2024_04_29_185428_create_tips_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tips', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->boolean('display_date')->default(false);
+            $table->string('img')->nullable();
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tips');
+    }
+};

c1ef4aad4811d6de86fe5f3833fbafcc0c920b15
Sélèna Boullée <sboullee@kalya-sante.com>
2024-04-29 21:11:49 +0200

    feat: add Categ plant and plants to database

---
+++ database/migrations/2024_04_29_190006_create_plants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('plants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->date('start_month');
+            $table->date('end_month');
+            $table->integer('watering_rythm');
+            $table->integer('growing_time');
+            $table->string('illness');
+            $table->string('img')->nullable();
+            $table->foreignIdFor(\App\Models\CategPlant::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('plants');
+    }
+};

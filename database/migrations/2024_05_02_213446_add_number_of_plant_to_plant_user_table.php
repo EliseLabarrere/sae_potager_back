3828d364d8dcf61f960964b164981ef93a5c4068
Sélèna Boullée <sboullee@kalya-sante.com>
2024-05-03 12:46:28 +0200

    feat: add column number_of_plant

---
+++ database/migrations/2024_05_02_213446_add_number_of_plant_to_plant_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('plant_user', function (Blueprint $table) {
+            $table->after('nickname', function ($table) {
+                $table->integer('number_of_plant');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('plant_user', function (Blueprint $table) {
+            $table->dropColumn('number_of_plant');
+        });
+    }
+};
